3a7913a884a9b210801f870752a66c33c0e21313
Alexander Zherdev <azherdev@yahoo.com>
2020-05-31 18:36:58 -0700

    refactor of files

--- scrub_test.go
+++ scrub_test.go
@@ -176,3 +176,64 @@ type Cat struct {
 	Name string `json:"name" acl:"root,account"`
 	Type string `json:"type" acl:"root"`
 }
+
+func TestInvalidType_Int(t *testing.T) {
+
+	var testVar int = 10
+	err := Scrub(testVar, []string{"owner"})
+	assert.Error(t, err)
+	err = Scrub(&testVar, []string{"owner"})
+	assert.Error(t, err)
+
+}
+
+func TestInvalidType_Float(t *testing.T) {
+
+	var testVar float32 = 10
+	err := Scrub(testVar, []string{"owner"})
+	assert.Error(t, err)
+	err = Scrub(&testVar, []string{"owner"})
+	assert.Error(t, err)
+
+}
+
+func TestInvalidType_Bool(t *testing.T) {
+
+	var testVar bool = true
+	err := Scrub(testVar, []string{"owner"})
+	assert.Error(t, err)
+	err = Scrub(&testVar, []string{"owner"})
+	assert.Error(t, err)
+
+}
+
+func TestInvalidType_InterfaceInderection(t *testing.T) {
+
+	var origVar bool = true
+	var testVar interface{} = origVar
+
+	err := Scrub(testVar, []string{"owner"})
+	assert.Error(t, err)
+	err = Scrub(&testVar, []string{"owner"})
+	assert.Error(t, err)
+}
+
+func TestInvalidType_Slice(t *testing.T) {
+
+	var testVar []int = []int{1, 2, 3, 4}
+
+	err := Scrub(testVar, []string{"owner"})
+	assert.Error(t, err)
+	err = Scrub(&testVar, []string{"owner"})
+	assert.Error(t, err)
+}
+
+func TestInvalidType_Map(t *testing.T) {
+
+	var testVar map[string]int = map[string]int{"one": 1, "two": 2, "three": 3}
+
+	err := Scrub(testVar, []string{"owner"})
+	assert.Error(t, err)
+	err = Scrub(&testVar, []string{"owner"})
+	assert.Error(t, err)
+}
